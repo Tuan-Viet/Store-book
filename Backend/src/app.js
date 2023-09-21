570f88aabd85b22281e17bd9cfc566da7a2c6d1f
TuanViet <vietlt226688@gmail.com>
2023-09-21 20:10:57 +0700

    CRUD Category & Product

---
+++ Backend/src/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/database";
+import categoryRouter from "./routes/category";
+import productRouter from "./routes/product";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Conect to MongoDB
+connectDB(process.env.MONGODB_URL)
+
+// middleware
+app.use("/api/categories", categoryRouter)
+app.use("/api/products", productRouter)
+
+export const viteNodeApp = app;
